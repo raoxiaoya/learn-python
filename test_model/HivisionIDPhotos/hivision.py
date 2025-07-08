db5caeb8ae90c7f9487d32c1b5b7d7fa22de7bd6
raoxiaoya <raoxiaoya@bookan.com>
2025-07-08 10:13:30 +0800

    add

--- test_model/HivisionIDPhotos/hivision.py
+++ test_model/HivisionIDPhotos/hivision.py
@@ -5,6 +5,6 @@ conda activate tensorflow2.19.0
 
 https://github.com/Zeyi-Lin/HivisionIDPhotos
 
-测试代码需要在 D:\dev\php\magook\trunk\server\HivisionIDPhotos 项目中运行。
+测试代码需要在 D:\dev\php\magook\trunk\server\HivisionIDPhotos 项目中运行。教程在 总结.md 中查看。
 
 '''
